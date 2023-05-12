571ff87cf2cabef92c9ad9ec290a8b1cddf37335
AndriiPets <nnggtrgftgd@gmail.com>
2023-05-12 23:54:17 +0300

    misc

--- utils/download_video.py
+++ utils/download_video.py
@@ -8,9 +8,7 @@ def download_from_url(url: str) -> str:
     if os.path.exists(PATH):
         return PATH
     yt = YouTube(url)
-
     video = yt.streams.filter(only_audio=True).first()
-
     out_file = video.download(filename=PATH)
 
     return out_file
